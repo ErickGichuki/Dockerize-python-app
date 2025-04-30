4923d87e86ea922d1f45c94e047d113a25f00f8d
+ <+>
2025-04-30 18:07:04 +0300

    refactor:modify the docs

--- devops/devops/settings.py
+++ devops/devops/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure--8k1taqn5t)mokljw&#ngh*dw4adwidl+(jri8dd^1l8iothg7"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
